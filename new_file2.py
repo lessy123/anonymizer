8b463508ec14a31ca9c89d414fe5b92c0ed27145
lessy123 <90840139+lessy123@users.noreply.github.com>
2022-05-29 22:53:34 +0300

    Delete new_file2.py

--- new_file2.py
+++
@@ -1,8 +0,0 @@
-import shutil # Подключаем модуль
-shutil.copyfile(r'/opt/RedDatabase/examples/mydatabase/test.fdb', r'/home/alex/worl/test.fdb')
-
-
-
-# my_file = open("/home/alex/work/BabyFile.txt", "w+")
-# my_file.write("Привет, файл!")
-# my_file.close()
